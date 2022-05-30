0bc84bd5397f63baf6259de5aa4b69ba81dcb6f7
Nelson Carvajal <nelcarca@gmail.com>
2022-05-30 00:37:14 -0400

    Creacion component de footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="text-center text-white fixed-bottom" style="background-color: black;">
+  <!-- Grid container -->
+  <div class="container p-2"></div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    Copyright © 2022 ParrillasToledo. Todos los derechos reservados. By Nelcarca
+  </div>
+  <!-- Copyright -->
+</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+a {
+   text-decoration: none;
+}
+</style>
