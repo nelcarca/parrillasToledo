0f738bc75e4d770d2b40d11e660a67b838ccf9aa
Nelson Carvajal <nelcarca@gmail.com>
2022-05-29 23:46:22 -0400

    Limpieza

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Layout>
+    <h3>Contenido de los Productos</h3>
+    
+  </Layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Layout from '@/layout/Layout.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Layout,
   }
 }
 </script>
