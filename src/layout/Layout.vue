dd29b6b31c42631dce640eddd9f2bc8c5a7a6a1d
Nelson Carvajal <nelcarca@gmail.com>
2022-05-29 23:50:51 -0400

    Creación de layout para cada pagina

---
+++ src/layout/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <!-- navbar -->
+    <Navbar/>
+    <div class="container mt-5">
+      <!-- aqui se van renderizar todos nuestros componentes -->
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
