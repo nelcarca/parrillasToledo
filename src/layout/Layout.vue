5b52051bf42b08e8791b9271d358c0b59317b601
Nelson Carvajal <nelcarca@gmail.com>
2022-05-30 00:36:20 -0400

    Integración de footer

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -6,15 +6,18 @@
       <!-- aqui se van renderizar todos nuestros componentes -->
       <slot/>
     </div>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
