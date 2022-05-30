e5b406f3d1b87323935a6b0e7d36d05749b7c0f6
Nelson Carvajal <nelcarca@gmail.com>
2022-05-29 23:49:42 -0400

    Creacion del navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+         <div class="logo">
+         <img
+            src="../assets/logoToledoParrillasAlternativOscuroRojo1080x367.png"
+            alt="Logo Parrillas Toledo"
+            >
+        </div>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">Inicio</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">Productos</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">Quinchos</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">Contacto</router-link>
+          </li>
+          
+        </ul>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+img {
+    width: 200px;
+  }
+.navbar {
+background-image: url(../assets/madera1.jpg);
+}
+</style>
